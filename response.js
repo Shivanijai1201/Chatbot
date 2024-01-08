113fe16ee41200cb270adb269c26b80b74f203d7
Shivani Jayshwal <129832121+Shivanijai1201@users.noreply.github.com>
2024-01-08 14:19:11 +0530

    added response.js

---
+++ response.js
@@ -0,0 +1,13 @@
+const responseObj = {
+    'Hello': "Hello Myself Chatbot, How can I help you?",
+    'I want to ask you some questions': "Of Course, Ask me",
+    'How are you': "I'm pretty good. Glad to see you. How are you doing?",
+    'I am doing great': "That's great!",
+    'Are you a Robot': "Yes, I'm a robot pretending to be a human.",
+    'What is javascript in brief': "JavaScript is a high-level, versatile programming language primarily used for creating dynamic and interactive content on websites. It can be executed in web browsers to enhance user experiences, handle user input, manipulate web page elements, and more.",
+    'What is python': "Python is a high-level, versatile, and easy-to-read programming language known for its simplicity and readability. It is widely used for a variety of applications, including web development, data analysis, artificial intelligence, and automation.",
+    "Tell me about today in detail": new Date().toDateString(),
+    "Tell me the time right now": new Date().toLocaleTimeString(),
+    "Tell me three applications of python": "Python is used in a wide range of applications. Here are three common examples: 1)Web Development, 2)Data Science and Analysis, 3)Artificial Intelligence and Machine Learning",
+    
+  };
